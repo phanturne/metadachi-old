03d520d7166ec19e7ff968fbaaa4fb47c92bdb5b
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2023-11-17 21:07:57 -0800

    Updating Supabase auth

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -5,12 +5,11 @@ import { createClient as createBrowserClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const createTauriClient = () =>
-  createBrowserClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-      persistSession: true,
-      storageKey: 'CustomApp',
-      // storage: window?.localStorage,
-      flowType: 'pkce',
-    },
-  });
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    storageKey: 'CustomApp',
+    // storage: window?.localStorage,
+    flowType: 'pkce',
+  },
+});
