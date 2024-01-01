39b0d33ca2776a8d455d6960f36dce65b2139a85
Kevin Ding <72530065+phanturne@users.noreply.github.com>
2024-01-01 15:50:12 -0800

    Editing default system template

--- src/constants.ts
+++ src/constants.ts
@@ -111,8 +111,6 @@ export const Google = {
 
 export const DEFAULT_INPUT_TEMPLATE = `{{input}}`; // input / time / model / lang
 export const DEFAULT_SYSTEM_TEMPLATE = `
-You are ChatGPT, a large language model trained by OpenAI.
-Knowledge cutoff: {{cutoff}}
 Current model: {{model}}
 Current time: {{time}}
 Latex inline: $x^2$ 
