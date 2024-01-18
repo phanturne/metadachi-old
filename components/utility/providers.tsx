503cfbd1e91e684e13d930c1abc755a72d098bf1
Kevin Ding <kding60@gatech.edu>
2024-01-17 22:13:22 -0800

    Adding JoyUI ThemeProvider & SnackbarProvider

--- components/utility/providers.tsx
+++ components/utility/providers.tsx
@@ -4,11 +4,17 @@ import { TooltipProvider } from "@/components/ui/tooltip"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { ThemeProviderProps } from "next-themes/dist/types"
 import { FC } from "react"
+import ThemeProvider from "@/lib/providers/ThemeProvider"
+import { SnackbarProvider } from "@/lib/providers/SnackbarProvider"
 
 export const Providers: FC<ThemeProviderProps> = ({ children, ...props }) => {
   return (
-    <NextThemesProvider {...props}>
-      <TooltipProvider>{children}</TooltipProvider>
-    </NextThemesProvider>
+    <ThemeProvider options={{ key: "joy" }}>
+      <SnackbarProvider>
+        <NextThemesProvider {...props}>
+          <TooltipProvider>{children}</TooltipProvider>
+        </NextThemesProvider>
+      </SnackbarProvider>
+    </ThemeProvider>
   )
 }
---
+++ lib/providers/SnackbarProvider.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import React, { createContext, useContext, useState } from "react"
+import { Button, ColorPaletteProp, Snackbar, VariantProp } from "@mui/joy"
+
+type SnackbarConfig = {
+  msg?: string
+  variant?: VariantProp
+  color?: ColorPaletteProp
+  action?: {
+    text: string
+    onClick: () => void
+  }
+  onClose?: () => void
+}
+
+// Create a context with initial values
+const SnackbarContext = createContext({
+  snackbar: {} as SnackbarConfig | undefined,
+  setSnackbar: (_: SnackbarConfig | undefined) => {}
+})
+
+// Context Provider
+export const SnackbarProvider = ({
+  children
+}: {
+  children: React.ReactNode
+}) => {
+  const [snackbar, setSnackbar] = useState<SnackbarConfig | undefined>(
+    undefined
+  )
+
+  // Pass the context values to the provider
+  const contextValue = { snackbar, setSnackbar }
+
+  return (
+    <SnackbarContext.Provider value={contextValue}>
+      <Snackbar
+        variant={snackbar?.variant ?? "soft"}
+        color={snackbar?.color ?? "neutral"}
+        size="sm"
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        autoHideDuration={3000}
+        open={snackbar !== undefined}
+        onClose={(_, reason) => {
+          if (reason === "clickaway") {
+            return
+          }
+          snackbar?.onClose?.()
+          setSnackbar(undefined)
+        }}
+      >
+        {snackbar?.msg}
+        {snackbar?.action && (
+          <Button
+            variant="plain"
+            onClick={() => {
+              snackbar.action?.onClick?.()
+            }}
+          >
+            {snackbar.action.text}
+          </Button>
+        )}
+      </Snackbar>
+      {children}
+    </SnackbarContext.Provider>
+  )
+}
+
+// Custom hook to use the SnackbarModalContext
+export const useSnackbar = () => {
+  return useContext(SnackbarContext)
+}
