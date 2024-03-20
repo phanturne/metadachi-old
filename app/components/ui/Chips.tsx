930d9621a84911155ae11d136ec64687d2113755
Kevin Ding <kding60@gatech.edu>
2024-03-19 23:27:38 -0700

    feat(Heart's Symphony): Adding layout for Heart's Symphony page. Updating Coming Soon Chip

--- app/components/ui/Chips.tsx
+++ app/components/ui/Chips.tsx
@@ -1,8 +1,14 @@
 import { Chip } from "@mui/joy"
+import { AccessAlarmRounded } from "@mui/icons-material"
 
 export default function ComingSoonChip() {
   return (
-    <Chip size="sm" variant="outlined" color="primary">
+    <Chip
+      size="sm"
+      variant="solid"
+      color="primary"
+      startDecorator={<AccessAlarmRounded />}
+    >
       Coming Soon
     </Chip>
   )
