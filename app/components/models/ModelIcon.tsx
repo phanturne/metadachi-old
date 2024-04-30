f86cb35a11b744d65c2ab975621d50aa831c8135
Kevin Ding <kding60@gatech.edu>
2024-04-29 19:16:40 -0700

    feat: Reduce model provider font size

--- app/components/models/ModelIcon.tsx
+++ app/components/models/ModelIcon.tsx
@@ -12,12 +12,16 @@ export const ModelIcon: FC<ModelIconProps> = ({
   size
   // ...props
 }) => {
+  // Some icons are inherently big, so use a smaller font for them
+  let bigIconFontSize = "text-xl"
   let fontSize = "text-2xl"
   switch (size) {
     case "sm":
+      bigIconFontSize = "text-xl"
       fontSize = "text-2xl"
       break
     case "md":
+      bigIconFontSize = "text-2xl"
       fontSize = "text-3xl"
       break
   }
@@ -25,26 +29,31 @@ export const ModelIcon: FC<ModelIconProps> = ({
   switch (provider as ModelProvider) {
     case "openai":
       return (
-        <Icon icon="logos:openai-icon" className={`${fontSize} dark:invert`} />
+        <Icon
+          icon="logos:openai-icon"
+          className={`${bigIconFontSize} dark:invert`}
+        />
       )
     case "mistral":
-      return <Icon icon="logos:mistral-ai-icon" className={fontSize} />
+      return <Icon icon="logos:mistral-ai-icon" className={bigIconFontSize} />
     case "groq":
-      return <Icon icon="solar:stars-bold-duotone" className={fontSize} />
+      return (
+        <Icon icon="solar:stars-bold-duotone" className={bigIconFontSize} />
+      )
     case "anthropic":
       return (
         <Icon
           icon="simple-icons:anthropic"
-          className={`${fontSize} light:invert`}
+          className={`${bigIconFontSize} light:invert`}
         />
       )
     case "google":
-      return <Icon icon="logos:google-icon" className={fontSize} />
+      return <Icon icon="logos:google-icon" className={bigIconFontSize} />
     case "perplexity":
       return (
         <Icon
           icon="simple-icons:perplexity"
-          className={`${fontSize} light:invert`}
+          className={`${bigIconFontSize} light:invert`}
         />
       )
     default:
